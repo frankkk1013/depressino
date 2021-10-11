d899356b3040a32a780e968e41a7808101c1606d
Emanuele Giunta <egindustries11@gmail.com>
2021-10-11 10:32:49 +0200

    first push

---
+++ Shared/depressinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  depressinoApp.swift
+//  Shared
+//
+//  Created by Emanuele Giunta on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct depressinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
